3e50d279d9469351c429e07547a3eac49875c5c8
Bobik77 <75545873+Bobik77@users.noreply.github.com>
2021-04-07 14:59:27 +0200

    vivado project create

---
+++ DE1_project/DE1_project.srcs/sources_1/new/sensor_driver.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.04.2021 14:58:57
+-- Design Name: 
+-- Module Name: sensor_driver - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sensor_driver is
+--  Port ( );
+end sensor_driver;
+
+architecture Behavioral of sensor_driver is
+
+begin
+
+
+end Behavioral;
