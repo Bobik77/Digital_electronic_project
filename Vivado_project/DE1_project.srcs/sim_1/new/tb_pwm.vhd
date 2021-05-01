3da7d1bd86f16316deb0686bcac08e9259a4f871
Samuelturak <xturak01@vutbr.cz>
2021-05-01 22:39:21 +0200

    pwm commented

--- Vivado_project/DE1_project.srcs/sim_1/new/tb_pwm.vhd
+++ Vivado_project/DE1_project.srcs/sim_1/new/tb_pwm.vhd
@@ -1,35 +1,29 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Turák Samuel
 -- 
 -- Create Date: 04/19/2021 02:23:53 PM
 -- Design Name: 
 -- Module Name: tb_pwn_gen - Behavioral
--- Project Name: 
+-- Project Name: DE1 Project
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
--- 
+--      testbench for pwm module
 -- Dependencies: 
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--      Ideal simulation time for these settings is 30 microseconds
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
+use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity tb_pwm is
     
@@ -40,7 +34,7 @@ end tb_pwm;
 architecture Behavioral of tb_pwm is
 
         
-        constant c_clk_period       : time := 10 ns;
+        constant c_clk_period       : time := 10 ns; -- period of pwm signal
         signal s_clk                : std_logic;
         signal s_duty               : unsigned(8 - 1 downto 0);
         signal s_output             : std_logic;
@@ -55,9 +49,10 @@ begin
         output      => s_output
         );
         
+        -- clock signal generation
      p_clk_gen : process
         begin
-        while now < 10 ms loop   -- 1 sec of simulation
+        while now < 10 ms loop   -- 10 milisec of simulation
             s_clk <= '0';
             wait for c_clk_period / 2;
             s_clk <= '1';
@@ -66,14 +61,14 @@ begin
         wait;
     end process p_clk_gen;
     
-    p_duty : process  -- LUT signal recreation
+    p_duty : process  -- recreation of the input signal
        begin
-           s_duty <= "00000011"; wait for 2550 ns;
+           s_duty <= "00000011"; wait for 2550 ns;  -- the original freq is dependent on the sampling freq...
                                                 
-           s_duty <= "00001001"; wait for 2550 ns;
+           s_duty <= "00001001"; wait for 2550 ns;  -- ... here we use this freq for clarity of the signal
                                                 
            s_duty <= "00111111"; wait for 2550 ns;
-                                                
+           
            s_duty <= "01111111"; wait for 2550 ns;
                                                 
            s_duty <= "10011111"; wait for 2550 ns;
--- Vivado_project/DE1_project.srcs/sources_1/new/pwm.vhd
+++ Vivado_project/DE1_project.srcs/sources_1/new/pwm.vhd
@@ -1,70 +1,64 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Turák Samuel
 -- 
 -- Create Date: 04/26/2021 09:06:13 PM
--- Design Name: 
+-- Design Name: pwm.vhd
 -- Module Name: pwm - Behavioral
--- Project Name: 
+-- Project Name: DE1 Project
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
--- 
+--      Pulse Width Modulation for output signal
 -- Dependencies: 
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--      Input is a sampled 8b analog signal
+--      Output is a rectangular signal with changing duty cycle
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
+use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity pwm is
     port (
-  
-    clk           : in std_logic;
-    duty          : in unsigned(8 - 1 downto 0);
-    output        : out std_logic
-      
+    clk           : in std_logic; -- clock signal 100MHz
+    duty          : in unsigned(8 - 1 downto 0); -- 8b vector
+    output        : out std_logic -- pwm signal
   );
+  
 end pwm;
 
 architecture Behavioral of pwm is
-    signal s_cnt_local : natural;
+    signal s_cnt_local : natural;   
     
 begin
-    
+
+    -- Pulse width generation process
     pwm : process(clk)
         begin
             if rising_edge(clk) then
                 
-                if (s_cnt_local >= duty) then
-                    output <= '0';
+                if (s_cnt_local >= duty) then -- when the signal reaches the gicen duty cycle...
+                    output <= '0'; -- ... only then it gets low, ...
                 else
-                    output <= '1';
+                    output <= '1'; -- ... before it is up
                 end if;
                 
-                s_cnt_local <= s_cnt_local +1;
+                s_cnt_local <= s_cnt_local +1; -- incrementing the counter
                 
-                if (s_cnt_local = 255) then
-                    s_cnt_local <= 0;
-                    output <= '1';
+                if (s_cnt_local = 255) then -- we let the counter reach the whole period...
+                    s_cnt_local <= 0; -- ... then it gets low and the process repeats
+                    output <= '1'; -- setting the output back to 1
                 end if;
             end if;
         
         end process pwm;    
-        
-                  
+             
 end Behavioral;
